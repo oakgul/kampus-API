87f7cf77a8e383f527e1b21b835d0dcc1fda3dc9
oakgul <orhan.akgull@gmail.com>
2020-09-22 20:48:52 +0300

    Cors package added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const connectDatabase = require('./helpers/connectDatabase');
 const customErrorHandler = require('./middlewares/customErrorHandler');
 const path = require('path');
+const cors =  require('cors');
 
 const routers = require('./routers/index');
 
@@ -15,6 +16,9 @@ dotenv.config({
 connectDatabase();
 
 const app = express();
+
+// CORS setting
+app.use(cors());
 
 // req.body - consolda gözükmesi için (Eskiden bodyparser kullanıyorduk ama artık express ile geliyor)
 app.use(express.json());
