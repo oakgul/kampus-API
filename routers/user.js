290d108ebe991ff5d8fde518457d606528387996
oakgul <orhan.akgull@gmail.com>
2020-09-23 03:07:39 +0300

    announce and user endpoint added getAccessToRoute middlewares

--- routers/user.js
+++ routers/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { getSingleUser, getAllUsers } = require('../controllers/user.js');
 const { checkUserExist } = require('../middlewares/databaseErrorHelpers');
+const { getAccessToRoute } = require('../middlewares/auth');
 
 const router = express.Router();
 
-router.get('/:id', checkUserExist, getSingleUser);
-router.get('/', getAllUsers);
+router.get('/:id',getAccessToRoute, checkUserExist, getSingleUser);  
+router.get('/',getAccessToRoute, getAllUsers);
 
 module.exports = router;
